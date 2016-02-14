8d40ddb63ce665fc5d4d77bbeef8f42e369f76c4
Mattias Larsson <mattias.larsson75@outlook.com>
2016-02-15 00:55:02 +0100

    Lade till en ny form

---
+++ src/shapes/Snabel.java
@@ -0,0 +1,25 @@
+package shapes;
+
+import javafx.scene.paint.Color;
+
+public class Snabel extends Form {
+
+	@Override
+	public String getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Color getTextColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
